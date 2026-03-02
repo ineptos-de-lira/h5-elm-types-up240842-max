8395dbac4877bd2570077d3b1947d71230e532d4
yaelaketzali <up240842@alumnos.upa.edu.mx>
2026-03-01 20:21:26 -0600

    Tarea 5 completada

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,67 @@
 module Helper exposing (..)
+
+import Html exposing (main_)
+
+
+
+-- EJERCICIO 1
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus grade =
+    if grade < 0 then
+        Pending
+
+    else if grade >= 7 then
+        Approved
+
+    else
+        Failed
+
+
+
+-- EJERCICIO 2.1
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+
+-- EJERCICIO 2.2
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction domain =
+    case domain of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Boarding ->
+            "Buscar boleto"
+
+        _ ->
+            "Esperar"
+
+
+
+-- EJERCICIO 2.3
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction lista =
+    List.map airplaneScheduleAction lista
